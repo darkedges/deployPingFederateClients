321cb940134a6d6f9b1a5e576b5287974ea416c8
DarkEdges <nirving.darkedges@darkedges.com>
2026-08-15 10:21:38 +1000

    update

--- setup/outputs.tf
+++ setup/outputs.tf
@@ -7,3 +7,16 @@ output "github_repository_variables" {
 output "backend_config" {
   value = { bucket = aws_s3_bucket.terraform.id, region = var.aws_region, kms_key_id = aws_kms_key.terraform.arn, encrypt = true, use_lockfile = true }
 }
+
+output "github_oidc_trust" {
+  description = "GitHub OIDC audience and subjects accepted by each AWS deployment role."
+  value = {
+    for environment, branch in local.environments : environment => {
+      audience = "sts.amazonaws.com"
+      subjects = [
+        "repo:${var.github_repository}:ref:refs/heads/${branch}",
+        "repo:${var.github_repository}:environment:${environment}",
+      ]
+    }
+  }
+}
